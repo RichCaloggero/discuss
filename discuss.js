805d4d0fc6c6798a0abec6c6de347509e1c4dc3b
Rich Caloggero <rjc@mit.edu>
2024-01-13 18:49:50 -0500

    * input form works
    * up / down voting works

--- discuss.js
+++ discuss.js
@@ -24,6 +24,9 @@ this.#id = generateId(this);
 } // constructor
 
 addStudent (s) {this.#students.push(s);}
+add (obj) {
+return this.addStudent(new Student(obj.name));
+} // add
 
 toString () {
 return `<div class="discussion actor" id="${this.#id}">
@@ -46,10 +49,12 @@ class Student {
 #id = "";
 get id () {return this.#id;}
 #name = "";
+get name () {return this.#name;}
 #votes = 0;
 #documents = [];
-get name () {return this.#name;}
 get documents () {return this.#documents;}
+#fields = ["name"];
+get fields () {return this.#fields;}
 
 constructor (name) {
 this.#id = generateId(this);
@@ -57,6 +62,9 @@ this.#name = name;
 } // constructor
 
 addDocument (d) {this.#documents.push(d);}
+add (obj) {
+return this.addDocument(new Document(obj.title, obj.url));
+} // add
 
 toString () {
 return `<div class="student actor" id="${this.#id}">
@@ -83,6 +91,8 @@ get id () {return this.#id;}
 #votes = 0;
 #comments = [];
 get comments () {return this.#comments;}
+#fields = ["title", "url"];
+get fields () {return this.#fields;}
 
 constructor (title, url) {
 this.#id = generateId(this);
@@ -93,6 +103,9 @@ this.#url = url;
 upVote () {this.#votes += 1;}
 downVote () {if (this.#votes > 0) this.#votes -= 1;}
 addComment (c) {this.#comments.push(c);}
+add (obj) {
+return addComment(new Comment(obj.student, obj.text));
+} // add
 
 toString () {
 return `<div class="document actor" id="${this.#id}">
@@ -123,6 +136,8 @@ get id () {return this.#id;}
 #votes = 0;
 #replies = [];
 get replies () {return this.#replies;}
+#fields = ["student", "text"];
+get fields () {return this.#fields;}
 
 
 constructor (author, text) {
@@ -134,6 +149,9 @@ this.#text = text;
 upVote () {this.#votes += 1;}
 downVote () {if (this.#votes > 0) this.#votes -= 1;}
 addComment (c) {this.#replies.push(c);}
+add (obj) {
+return addComment(new Comment(obj.student, obj.text));
+} // add
 
 toString () {
 return `<div class="comment actor" id="${this.#id}">
@@ -158,6 +176,15 @@ ${this.#replies.length === 0? "" : toList(this.#replies, "ol")}
 
 /// operators
 
+function not(x) {return !x;}
+
+function zip (names, values) {
+if (names.length !== values.length) throw new Error("zip: names and values must be same length");
+return Object.fromEntries(
+names.map((name, i) => [name, values[i]])
+); // fromEntries
+} // zip
+
 function toList (l, type = "ul") {
 return `<${type}><li>
 ${l.join("\n</li><li>\n")}
@@ -173,54 +200,91 @@ container.setAttribute("aria-label", container.className);
 } // addContainerLabels
 
 function actionHandler (e) {
+if (not(e.target.classList.contains("action"))) return;
 const action = e.target.dataset.action;
+if (not(action)) return;
 const actor = getActor(e.target.closest(".actor").id);
 const immediate = e.target.classList.contains("immediate");
-performAction(action, actor, immediate);
+performAction(action, actor, immediate, e.target);
 } // actionHandler
 
-function performAction (action, actor, immediate) {
+function performAction (action, actor, immediate, target) {
 console.log("performAction: ", action, actor, immediate);
 if (immediate) {
 actor[action]();
+rerender(actor);
 
 } else {
 // display form
-// get result and perform action
+displayForm(actor, action, target);
 } // if
 
-rerender(actor);
 } // performAction
+
+function displayForm (actor, action, target) {
+const dialog = document.querySelector("#get-info");
+const form = dialog.querySelector("form");
+dialog.hidden = false;
+const fieldNames = action === "addStudent" ? ["name"]
+: action === "addDocument"? ["title", "url"]
+: action === "addComment"? ["student", "text"]
+: null;
+const fields = [...form.querySelectorAll(".field")];
+fields.forEach(x => {
+x.hidden = not(fieldNames.includes(x.querySelector("[name]").name))
+});
+form.querySelector(".submit").hidden = false;
+
+form.addEventListener("submit", e => {
+const values = [...fields].filter(x => not(x.hidden))
+.map(x => x.querySelector("[name]").value);
+actor.add(zip(fieldNames, values));
+
+fields.forEach(x => x.hidden = true);
+form.querySelector(".submit").hidden = true;
+
+dialog.close();
+rerender(actor).querySelector(".action").focus();
+},
+{once: true}); // submit
+
+dialog.showModal();
+fields[0].focus();
+} // displayForm
+
 
 function rerender (actor) {
 const element = document.querySelector(`#${actor.id}`);
 console.log("rerendering ", actor, element);
 element.insertAdjacentHTML("beforeBegin", actor.toString());
+const result = element.previousElementSibling;
 element.remove();
+return result;
 } // rerender
 
 /// start
 
-const discussion = new Discussion();
+let discussion = new Discussion();
 function discuss (element = document.body) {
 element.insertAdjacentHTML("afterBegin", 
 `<dialog id="get-info" hidden>
-<form>
+<form method="dialog">
 <div class="field" hidden><label>Name: <input name="name"></label></div>
 <div class="field" hidden><label>Title: <input name="title"></label></div>
 <div class="field" hidden><label>URL: <input name="url"></label></div>
 <div class="field" hidden><label>Student: <select name="student"></select></label></div>
 <div class="field" hidden><label>Comment: <input name="comment"></label></div>
-<div class="field" hidden><button type="submit">Add</button></div>
+<div class="submit" hidden><button type="submit">Add</button></div>
 </form>
 </dialog>
 
 ${discussion}
 `); // insertAdjacentHTML
+
 element.addEventListener("click", actionHandler);
+return element;
 } // discuss
 
-$discussion = discuss(); // $discussion is the dom root
 
 
 /// test data
